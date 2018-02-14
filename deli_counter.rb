78707ab2082b54873292780440924ee20091639a
Berry  Semexan <bsemexan@gmail.com>
2018-02-14 02:07:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,17 @@
 # Write your code here.
-def take_a_number(katz_deli, customer)
-  katz_deli.push(customer)
-  puts "Welcome, #{customer}. You are number #{katz_deli.size} in line."
-end
-
 def line(katz_deli)
-  if katz_deli.size == 0
+  if katz_deli.length == 0
     puts "The line is currently empty."
   else
     whos_in_line = "The line is currently:"
     katz_deli.each_with_index {|rm, idx| whos_in_line << " #{idx + 1}. #{rm}"}
   end
   puts whos_in_line
+end
+
+def take_a_number(katz_deli, customer)
+  katz_deli.push(customer)
+  puts "Welcome, #{customer}. You are number #{katz_deli.size} in line."
 end
 
 def now_serving(katz_deli)
