1c571ff0df6648556ab132ab8fed5a2b05b81ee5
Berry  Semexan <bsemexan@gmail.com>
2018-02-14 02:12:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,6 @@ def line(katz_deli)
   if katz_deli.length > 0
     whos_in_line = "The line is currently:"
     katz_deli.each_with_index {|rm, idx| whos_in_line << " #{idx + 1}. #{rm}"}
-    
   else
     puts "The line is currently empty."
   end
