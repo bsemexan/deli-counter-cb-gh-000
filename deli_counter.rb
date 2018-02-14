6d0ede6a96a68791781012fdc732a080719500e3
Berry  Semexan <bsemexan@gmail.com>
2018-02-14 01:24:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,16 +2,16 @@
 def take_a_number(katz_deli, customer)
 end
 
-def now_serving(katz_deli)
-  if katz_delit.length == 0
-    puts  "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{katz_deli.shift}."
-  end
-end
-
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
+  end
+end
+
+def now_serving(katz_deli)
+  if katz_delit.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift}."
   end
 end
