023044cac333eab78001b05a1feeda347c627d82
Berry  Semexan <bsemexan@gmail.com>
2018-02-14 01:03:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def take_a_number(katz_deli, customer)
 end
 
 def now_serving(katz_deli)
-  if katz_deli == 0
+  if katz_delit.length == 0
     puts  "There is nobody waiting to be served!"
   end
 end
