4002a56a1cfd7081fa09a87d8d2955d2e0f22dfe
Berry  Semexan <bsemexan@gmail.com>
2018-02-14 01:56:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,8 +8,9 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    katz_deli.each_with_index.map {|rm, idx| "The line is currently: #{idx + 1}. #{rm}"}
+    aa << katz_deli.each_with_index.map {|rm, idx| "The line is currently: #{idx + 1}. #{rm}"}
   end
+  puts aa
 end
 
 def now_serving(katz_deli)
